a29061533297c8e1a68312b1edd43d49497de62e
mezentsevw <mezentsevw@github.com>
2025-05-12 16:02:22 +0300

    убраны не использумые компоненты для демонстрации правильное отображение cardList

--- src/components/DemoPanel/DemoPanel.tsx
+++ src/components/DemoPanel/DemoPanel.tsx
@@ -1,14 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './DemoPanel.css';
 import { CardListDemo } from '../CardList/CardListDemo';
-import ProgressBar, { ProgressBarDemo } from '../ProgressBar/ProgressBar';
-import { ProgressBarDemoComplex } from '../ProgressBar/ProgressBarDemoComplex';
-import DataTable, { DataTableDemo } from '../DataTable/DataTable';
-import Notification, { NotificationDemo } from '../Notification/Notification';
-import Chart, { ChartDemo } from '../Chart/Chart';
 
 interface DemoPanelProps {
-  componentType: 'CardList' | 'ProgressBar' | 'Notification' | 'DataTable' | 'Chart';
+  componentType: 'CardList';
 }
 
 const DemoPanel: React.FC<DemoPanelProps> = ({ componentType }) => {
@@ -28,14 +23,6 @@ const DemoPanel: React.FC<DemoPanelProps> = ({ componentType }) => {
     switch (componentType) {
       case 'CardList':
         return <CardListDemo />;
-      case 'ProgressBar':
-        return <ProgressBarDemoComplex />;
-      case 'DataTable':
-        return <DataTableDemo />;
-      case 'Notification':
-        return <NotificationDemo />;
-      case 'Chart':
-        return <ChartDemo />;
       default:
         return <div className="error-message">Неизвестный компонент: {componentType}</div>;
     }
